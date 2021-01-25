9f18dcd03aa13e124ce7a4a1d097daa81e7e5aa1
cmplxapps <cmplxapps@gmail.com>
2021-01-25 13:46:40 -0800

    Updated Distribution Info

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytools-cmplxapps",
+    name="pytools-ca",
     version="1.0.0",
     author="cmplxapps",
     author_email="cmplxapps@gmail.com",
