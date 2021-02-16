4aa962186d7541ef8be2a61ba4ca49e78f4ae02b
cmplxapps <cmplxapps@gmail.com>
2021-02-15 23:58:09 -0800

    Current Progress 2.15.2021

--- pytools/modules.py
+++ pytools/modules.py
@@ -1,1 +1,20 @@
-isimported = lambda module: (str(module).find('<module ') == 0 and str(module)[-1] == '>')
+class TemporaryImportMirrorObject:
+	class __MirrorInfoAttribute:
+		def __init__(self, string, *args, **kwargs):
+			
+		def __repr__(self, *args, **kwargs):
+			return 'TemporaryImportMirrorObject.MirrorInfoAttribute()'
+	def __init__(self, module, mirror_name=False, *args, **kwargs):
+		if not mirror_name:
+			mirror_name = module
+		exec(f'import {self.__mirror__} as {self.__mirror_name__}', globals())
+	def __repr__(self, *args, **kwargs):
+		return f'<TemporaryImportMirrorType object mirroring \'{self.__mirror__}\' (\'{self.__mirror_name__}\')>'
+	def __str__(self, *args, **kwargs):
+		return f'TemporaryImportMirrorObject({self.__mirror__}, {self.__mirror_name__})'
+	def __del__(self, *args, **kwargs):
+		exec(f'del {self.__mirror__}', globals())
+		del self
+	def unimport(self, *args, **kwargs):
+		self.__del__()
+isimported = lambda module: ((str(module).find('<module ') == 0 and str(module)[-1] == '>') and module in list(globals().keys()))
---
+++ pytools/pyclass.py
@@ -0,0 +1,5 @@
+__all__ = []
+__version__ = '1.0.0'
+#--------------------------------------------------------------------------------------------------
+def new(iclass, fp='.', newVariable=None, *args, **kwargs):
+	import os as ___CMPLXAPPS_PYTOOLS___OS_MODULE___
